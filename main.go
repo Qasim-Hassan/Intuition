66159dc033f2bf3e35f3a7c96739e62ab0b2f92e
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-09-19 11:09:23 +0500

    building

--- main.go
+++ main.go
@@ -2,7 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	tea "charm.land/bubbletea/v2"
 )
+
+type model struct {
+	msg string
+}
+
+func (m model) inti() tea.Cmd {
+	return nil
+}
 
 func main() {
 	fmt.Println("Welcome to Intuition")
