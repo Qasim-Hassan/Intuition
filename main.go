9e13e96dc13a14be38803a044d355fd5d28d4993
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-09-19 11:18:31 +0500

    structuring modules

--- main.go
+++ main.go
@@ -10,8 +10,22 @@ type model struct {
 	msg string
 }
 
-func (m model) init() tea.Cmd {
+func (m model) Init() tea.Cmd {
 	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	return tea.View{Content: "baigan"}
+}
+
+func initializeMode() model {
+	return model{
+		msg: "ayo",
+	}
 }
 
 func main() {
