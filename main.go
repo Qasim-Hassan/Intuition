86736b9df943e0c487ff65d88f12999d07df0073
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-09-19 12:28:53 +0500

    working on terminal UI

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	tea "charm.land/bubbletea/v2"
 )
@@ -15,11 +16,27 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyPressMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
 	return m, nil
 }
 
 func (m model) View() tea.View {
-	return tea.View{Content: "baigan"}
+	return tea.View{Content: m.msg}
 }
 
 func initializeMode() model {
@@ -29,5 +46,10 @@ func initializeMode() model {
 }
 
 func main() {
-	fmt.Println("Welcome to Intuition")
+	p := tea.NewProgram(initializeMode())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error occured: %v", err)
+		os.Exit(1)
+	}
 }
